274b10a877192a83686f98b0169fb33548d18c4d
Sniff21 <rportonov@mail.ru>
2017-08-06 19:00:00 +0300

    BugFix#1

--- casino_private.h
+++ casino_private.h
@@ -5,19 +5,19 @@
 #define CASINO_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"5.1.0.32"
+#define VER_STRING	"5.0.0.17"
 #define VER_MAJOR	5
-#define VER_MINOR	1
+#define VER_MINOR	0
 #define VER_RELEASE	0
-#define VER_BUILD	32
+#define VER_BUILD	17
 #define COMPANY_NAME	"Sniff.inc"
-#define FILE_VERSION	"5.1.0.32"
+#define FILE_VERSION	"5.0.0.17"
 #define FILE_DESCRIPTION	"Casino Game стань магнатом!"
 #define INTERNAL_NAME	"Sniff.inc"
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	"Casino Game"
 #define PRODUCT_NAME	"Casino Game"
-#define PRODUCT_VERSION	"5.1.0.32"
+#define PRODUCT_VERSION	"5.0.0.17"
 
 #endif /*CASINO_PRIVATE_H*/
